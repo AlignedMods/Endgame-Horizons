0888dd310cd770dc6531b39462e8a7b4c66ade2f
AlignedMods <aligneddoesstuff@gmail.com>
2024-09-06 12:35:54 +0200

    removed the crafting recipes for wooden tools, made custom splashes and make ghasts invisible

--- src/main/java/net/aligned/survivaloverhaul/block/ModBlocks.java
+++ src/main/java/net/aligned/survivaloverhaul/block/ModBlocks.java
@@ -1,21 +1,24 @@
 package net.aligned.survivaloverhaul.block;
 
+import com.mojang.serialization.Lifecycle;
 import net.aligned.survivaloverhaul.SurvivalOverhaul;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.block.Block;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.spongepowered.asm.mixin.Overwrite;
 
 public class ModBlocks {
-    private static Block RegisterBlock(String name, Block block, ItemGroup tab) {
+    public static Block RegisterBlock(String name, Block block, ItemGroup tab) {
         RegisterBlockItem(name, block, tab);
         return Registry.register(Registry.BLOCK, new Identifier(SurvivalOverhaul.MOD_ID, name), block);
     }
 
-    private static Item RegisterBlockItem(String name, Block block, ItemGroup tab) {
+    public static Item RegisterBlockItem(String name, Block block, ItemGroup tab) {
         return Registry.register(Registry.ITEM, new Identifier(SurvivalOverhaul.MOD_ID, name),
                 new BlockItem(block, new FabricItemSettings().group(tab)));
     }
